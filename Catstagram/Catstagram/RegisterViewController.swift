3ed5aa748987f651958a8bb1460bd821e1646bdd
Kim Doy <heydoyhey@gmail.com>
2022-03-30 21:40:42 +0900

    회원가입 - 유효성검사 로직

--- Catstagram/Catstagram/RegisterViewController.swift
+++ Catstagram/Catstagram/RegisterViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 class RegisterViewController: UIViewController {
     // MARK: - Properties
     //  유효성 검사를 위한 프로퍼티
-    var isValidEmail = false
+    var isValidEmail = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
+
+    var isValidName = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
-    var isValidName = false
+    var isValidNickname = false {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
-    var isValidNickname = false
-    
-    var isValidPasssword = false
+    var isValidPasssword = false  {
+        didSet {
+            self.validateUserInfo()
+        }
+    }
     
     // Textfields
     @IBOutlet weak var emailTextField: UITextField!
@@ -26,6 +42,9 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var nicknameTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
     
     //  computed property로 처리하기
     var textFields: [UITextField] {
@@ -47,13 +66,15 @@ class RegisterViewController: UIViewController {
         let text = sender.text ?? ""
         switch sender {
         case emailTextField:
-            print("email")
+            self.isValidEmail = text.isValidEmail()
         case nameTextField:
-            print("name")
+            // 글자수만 2자 이상으로 확인
+            self.isValidName = text.count > 2
         case nicknameTextField:
-            print("nickname")
+            // 글자수만 2자 이상으로 확인
+            self.isValidNickname = text.count > 2
         case passwordTextField:
-            print("password")
+            self.isValidPasssword = text.isValidPassword()
         default:
             fatalError("Missing TextField")
         }
@@ -79,7 +100,47 @@ class RegisterViewController: UIViewController {
 //        emailTextField.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
 
     }
-    
-
-
+    // 사용자가 입력한 회원정보를 확인하고 UI 업데이트(가입 버튼 색상 변경)
+    private func validateUserInfo() {
+        if isValidEmail
+            && isValidName
+            && isValidNickname
+            && isValidPasssword {
+            self.signupButton.isEnabled = true
+            UIView.animate(withDuration: 0.33) {
+                self.signupButton.backgroundColor = UIColor(named: "facebookColor")
+            }
+        } else {
+            self.signupButton.isEnabled = false
+            UIView.animate(withDuration: 0.33) {
+                self.signupButton.backgroundColor = UIColor(named: "disabledButtonColor")
+            }
+        }
+        
+    }
 }
+
+// 정규표현식
+extension String {
+    
+    func isValidPassword() -> Bool {
+        // 대문자 , 소문자, 특수문자, 숫자 8자 이상일 때 True
+        let regularExpression  = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[\\d])(?=.*[~!@#\\$%\\^&\\*])[\\w~!@#\\$%\\^&\\*]{8,}$"
+
+        let passwordValidation = NSPredicate.init(format: "SELF MATCHES %@", regularExpression)
+        
+        return passwordValidation.evaluate(with: self)
+    }
+    
+    
+    func isValidEmail() -> Bool {
+        // @ 2글자 이상
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluate(with: self)
+    }
+    
+}
+
+
